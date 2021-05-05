43c576e8a30ab8d702ab959afc006cd9cb21d41d
MuhammadAryaSeptaKovitra <221910940@stis.ac.id>
2021-05-05 17:05:10 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hai</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aut, labore ab corrupti nesciunt ex voluptates laboriosam, nostrum soluta ipsa aliquid molestiae eaque magnam exercitationem dolores aliquam sint vel rem.</p>
 </body>
 </html>
