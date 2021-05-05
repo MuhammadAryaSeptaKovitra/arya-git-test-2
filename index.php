b26daaf5d1bec16b8ccb3509230ba9d21aa35899
MuhammadAryaSeptaKovitra <221910940@stis.ac.id>
2021-05-05 17:09:12 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
 <body>
     <h1>Hai</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aut, labore ab corrupti nesciunt ex voluptates laboriosam, nostrum soluta ipsa aliquid molestiae eaque magnam exercitationem dolores aliquam sint vel rem.</p>
+    <ul>
+        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum deleniti accusantium iste laboriosam voluptas nemo ea libero ipsum corrupti. Reiciendis possimus pariatur aut voluptatibus delectus, recusandae ipsam vitae adipisci eaque.</li>
+    </ul>
 </body>
 </html>
